179da709485c09fc2a851f754228079788a0e759
Jim Song <jim.song10@t-mobile.com>
2020-02-18 10:55:38 -0500

    Adding Unity first person view

---
+++ T-Rover-FPV-Unity/Assets/Scripts/GET_Stream.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+ 
+ public class GET_Stream : MonoBehaviour
+{
+    public Texture texture;
+    public InputField urlInput;
+    private string url;
+
+    public float GetRate = 0.15f;
+    private float nextGet = 0;
+
+    IEnumerator GetTexture()
+    {
+        //url = urlInput.text;
+        Renderer renderer = GetComponent<Renderer>();
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://192.168.1.176:8080/shot.jpg");
+        yield return www.Send();
+        renderer.material.mainTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+    }
+
+    void Update()
+    {
+        if (Time.time > nextGet)
+        {
+            StartCoroutine(GetTexture());
+            nextGet = Time.time + GetRate;
+            //Debug.Log("Hello");
+        }
+    }
+}
---
+++ T-Rover-FPV-Unity/README.md
@@ -0,0 +1,11 @@
+This is a sample Unity3D Project displaying video from an Mjpg stream.
+
+To use:
+
+1. Open "Mjpg Viewer/Assets/Scenes/Sample Mjpeg Viewer.unity" in Unity3D.
+
+2. Select the plane acting as the video screen and edit the url parameter to display a live MJPEG stream (you can use these to test with https://febon.blogspot.com/2013/02/public-free-ip-address-for-motion-jpeg.html ). The url should end in .mjpg
+
+3. Play the Sample Mjpeg Viewer scene and wait a few seconds for the MJPEG stream to connect.
+
+Enjoy!
